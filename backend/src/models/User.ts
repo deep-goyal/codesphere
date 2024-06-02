168c975251f90df82bd8909e9dff8e7d09228cbd
Deep Goyal <deep.goyal@icloud.com>
2024-06-03 00:28:25 +0530

    add user model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from "typeorm";
+import bcrypt from "bcryptjs";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ unique: true })
+  username!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  async comparePassword(password: string) {
+    return await bcrypt.compare(password, this.password);
+  }
+}
