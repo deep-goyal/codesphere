c54ee933e3978e52d9bf2ad53a8a6e96b65cbacf
Deep Goyal <deep.goyal@icloud.com>
2024-06-05 17:07:06 +0530

    add jwt auth for login

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { AppDataSource } from "../config/data-source";
 import { User } from "../models/User";
+import jwt from "jsonwebtoken";
 
 //init router
 const router = express.Router();
@@ -13,15 +14,48 @@ router.post("/register", async (req, res) => {
     const user = userRepository.create({ username, email, password });
 
     await userRepository.save(user);
-    res.status(201).json({ message: "user registered successfully!" });
+    res.status(201).json({ message: "User registered successfully." });
   } catch (err: any) {
     res.status(500).json({ error: err.message });
   }
 });
 
 //login route
-router.post("/login", (req, res) => {
-  res.send("This is the login endpoint");
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    //get the database repo
+    const userRepository = AppDataSource.getRepository(User);
+
+    //find the user's email
+    const user = await userRepository.findOneBy({ email });
+
+    //email not found
+    if (!user) return res.status(404).json({ message: "User not found." });
+
+    //if found, try matching password
+    const isMatch = await user.comparePassword(password);
+
+    //password incorrect
+    if (!isMatch)
+      return res
+        .status(400)
+        .json({ message: "Credential authentication failed." });
+
+    //if password match was successful, define the payload and generate a jwt token
+    const payload = { id: user.id, username: user.username };
+    const token = jwt.sign(
+      payload,
+      process.env.JWT_SECRET || "jwt_secret --- revisit",
+      { expiresIn: "1h" }
+    );
+
+    //send the token to the user
+    res.status(200).json({ token });
+  } catch (error: any) {
+    //server error
+    res.status(500).json({ error: error.message });
+  }
 });
 
 export default router;
