ec32f2ed5c6d5bbe459ac7bacf5d0303eb4145c1
Deep Goyal <deep.goyal@icloud.com>
2024-06-08 23:11:13 +0530

    comments and cleanup

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -3,8 +3,10 @@ import { DataSource } from "typeorm";
 import { User } from "../models/User";
 import { config } from "dotenv";
 
+//setup dotenv vars
 config();
 
+//data source for app using postgres
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.POSTGRES_HOST,
